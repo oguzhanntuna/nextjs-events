035877171e7416d285e063a23b07e7c8343f5bdc
oguzhanntuna <oguzhana.tuna@gmail.com>
2022-08-07 15:38:53 -0400

    adding new input components and newsletter route

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -4,6 +4,7 @@ import { getFeaturedEvents, getEventById } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import Comments from '../../components/input/comments';
 
 const EventDetailPage = (props) => {
   const event = props.selectedEvent;
@@ -32,6 +33,7 @@ const EventDetailPage = (props) => {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </>
   );
 };
